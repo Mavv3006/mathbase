1329b1ce4a4eeb6a56e1045d19aea02267c0450d
Mavv <marvdeus@gmail.com>
2020-04-26 11:30:38 +0200

    renamed table and added user password

--- sql/create_database.sql
+++ sql/create_database.sql
@@ -13,9 +13,9 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT = @@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS = @@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION = @@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
 --
@@ -30,14 +30,16 @@ USE `mathbase`;
 -- Tabellenstruktur für Tabelle `tasks`
 --
 
-CREATE TABLE `tasks` (
-  `id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `description` text NOT NULL,
-  `solution` varchar(255) NOT NULL,
-  `created_at` datetime DEFAULT current_timestamp(),
-  `updated_at` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `exercise`
+(
+    `id`          int(11)      NOT NULL,
+    `user_id`     int(11)      NOT NULL,
+    `description` text         NOT NULL,
+    `solution`    varchar(255) NOT NULL,
+    `created_at`  datetime DEFAULT current_timestamp(),
+    `updated_at`  datetime DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -45,11 +47,14 @@ CREATE TABLE `tasks` (
 -- Tabellenstruktur für Tabelle `users`
 --
 
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `users`
+(
+    `id`       int(11)      NOT NULL,
+    `username` varchar(255) NOT NULL,
+    `email`    varchar(255) NOT NULL,
+    `password` varchar(255) not null
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
 
 --
 -- Indizes der exportierten Tabellen
@@ -58,17 +63,17 @@ CREATE TABLE `users` (
 --
 -- Indizes für die Tabelle `tasks`
 --
-ALTER TABLE `tasks`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `user_id` (`user_id`);
+ALTER TABLE `exercise`
+    ADD PRIMARY KEY (`id`),
+    ADD KEY `user_id` (`user_id`);
 
 --
 -- Indizes für die Tabelle `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `username` (`username`),
-  ADD UNIQUE KEY `email` (`email`);
+    ADD PRIMARY KEY (`id`),
+    ADD UNIQUE KEY `username` (`username`),
+    ADD UNIQUE KEY `email` (`email`);
 
 --
 -- AUTO_INCREMENT für exportierte Tabellen
@@ -77,14 +82,14 @@ ALTER TABLE `users`
 --
 -- AUTO_INCREMENT für Tabelle `tasks`
 --
-ALTER TABLE `tasks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `exercise`
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT für Tabelle `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints der exportierten Tabellen
@@ -93,10 +98,10 @@ ALTER TABLE `users`
 --
 -- Constraints der Tabelle `tasks`
 --
-ALTER TABLE `tasks`
-  ADD CONSTRAINT `foreignKey_users(id)` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+ALTER TABLE `exercise`
+    ADD CONSTRAINT `foreignKey_users(id)` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40101 SET CHARACTER_SET_CLIENT = @OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS = @OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION = @OLD_COLLATION_CONNECTION */;
