c2dae8c5439538b9cf998b007afabb8ca7707c61
Mavv <marvdeus@gmail.com>
2020-04-27 17:45:11 +0200

    changed to PostgreSQL database

--- database/Database.php
+++ database/Database.php
@@ -76,19 +76,12 @@ abstract class Database
     private function connect(): PDO
     {
         try {
-            $this->connection = new PDO("mysql:host=" . $this->hostname . ";dbname=" . $this->dbName . ";charset=utf8", $this->username, $this->password);
+            $this->connection = new PDO("pgsql:host=" . $this->hostname . ";dbname=" . $this->dbName . ";charset=utf8", $this->username, $this->password);
             $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
             echo "Connection Error: " . $e->getMessage();
         }
 
-        try {
-            $query = "SET lc_time_names = 'de_DE'";
-            $stmt = $this->connection->prepare($query);
-            $stmt->execute();
-        } catch (PDOException $e) {
-            printf($e->getMessage());
-        }
         return $this->connection;
     }
 }
