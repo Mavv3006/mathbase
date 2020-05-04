8744e32e8caa740fdcb58068cf1a4d4014275e53
Luisa Fuss <luisa.fuss@web.de>
2020-05-04 16:24:34 +0200

    Head auslagern

---
+++ html/head.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<head>
+    <title><?=$site_name?></title>
+    <link rel="stylesheet" href="/css/materialize.css">
+    <meta charset="UTF-8">
+    <meta name="description" content="Lernplattform für Mathe">
+    <meta name="keywords" content="Mathe, Mathematik, Lernen, Üben, Schule">
+    <meta name="author" content="Mathbase">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+    $site_name = "Mathbase";
+    include 'html/head.php';
+?>
+
+<body>
+Hallo und Herzlich Willkommen bei Mathbase!
+
+<?php include 'html/forms/login.php';?>
+<?php include 'html/footer.php';?>
+
+</body>
