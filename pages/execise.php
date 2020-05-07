7365bbc10da31f88bcb71085e46605750664f12f
Mavv <marvdeus@gmail.com>
2020-05-07 08:42:27 +0200

    added js for checking the solution

--- pages/execise.php
+++ pages/execise.php
@@ -63,19 +63,19 @@ include_once("../html/head.php");
 
         ?>
 
-        <form action="" method="post" name="solutionForm">
-            <input type="hidden" name="id" value="<?= $_GET['id'] ?>" >
-            <input type="hidden" name="correct_solution" value="<?= $exercise->get_solution() ?>" >
+        <form action="" method="post" name="solutionForm" id="solutionForm">
+            <input type="hidden" name="id" value="<?= $_GET['id'] ?>">
+            <input type="hidden" name="correct_solution" value="<?= $exercise->get_solution() ?>">
             <div>
                 <div class="input-field">
                     <input type="text" name="solution" id="solution">
                     <label for="solution" id="solution_label">Antwort</label>
                 </div>
                 <span class="icon">
-                    <span class="done-icon ">
+                    <span class="done-icon display_none" id="done-icon">
                         <i class="material-icons done">done</i>
                     </span>
-                    <span class="clear-icon">
+                    <span class="clear-icon display_none" id="clear-icon">
                         <i class="material-icons clear">clear</i>
                     </span>
                 </span>
@@ -98,12 +98,24 @@ include_once("../html/head.php");
     <script>
         $('#show_solution').on('click', function() {
             let value = document.getElementById('solution').value;
-            let id = document.solutionForm.elements['id'].value;
             let solution = document.solutionForm.elements['correct_solution'].value
             console.log(solution);
             document.getElementById('solution_label').className = "active";
             document.getElementById('solution').value = solution;
         });
+
+        $('#solutionForm').submit(function(event) {
+            let user_solution = document.getElementById('solution').value;
+            let correct_solution = document.solutionForm.elements['correct_solution'].value
+            if (user_solution == correct_solution) {
+                document.getElementById('done-icon').classList.remove("display_none");
+                document.getElementById('clear-icon').classList.add("display_none");
+            } else {
+                document.getElementById('clear-icon').classList.remove("display_none");
+                document.getElementById('done-icon').classList.add("display_none");
+            }
+            event.preventDefault();
+        })
     </script>
 
 </body>
