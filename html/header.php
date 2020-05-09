6cbf87d8ca88d078dbf8121efd1daa08bf661f99
unknown <florian.goelz@outlook.de>
2020-05-09 13:08:35 +0200

    added <header> tag

--- html/header.php
+++ html/header.php
@@ -1,32 +1,36 @@
-<link rel="stylesheet" href="/css/header.css"/>
-<nav>
-    <div class="nav-wrapper">
-        <a href="#" class="brand-logo logo-wrapper">
-            <img id="header-logo" src="/assets/logo/logo.svg"/>
-            <div class="logo-text">
-                Math<span id="mb-orange">base</span>
-            </div>
-        </a>
-        <ul id="nav-mobile" class="right hide-on-med-and-down">
-            <?php 
-            if(false){
-            ?>
-                <li><a class="waves-effect btn" id="login-button">Einloggen</a></li>
-                <li><a class="waves-effect waves-light btn">Registrieren</a></li>
-            <?php
-            }else{
-            ?>
-                <li><a class="waves-effect waves-light btn">Aufgabe erstellen</a></li>
-                <li>
-                    <div class="avatar-container">
-                        <a class="avatar" href="#">
-                            <img class="avatar" src="/assets/pp_default.svg">
-                        </a>
-                    </div>
-                </li>
-            <?php
-            }
-            ?>
-        </ul>
-    </div>
-</nav>
+<header>
+    <link rel="stylesheet" href="/css/header.css"/>
+</header>
+<body>
+    <nav>
+        <div class="nav-wrapper">
+            <a href="#" class="brand-logo logo-wrapper">
+                <img id="header-logo" src="/assets/logo/logo.svg"/>
+                <div class="logo-text">
+                    Math<span id="mb-orange">base</span>
+                </div>
+            </a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <?php 
+                if(false){
+                ?>
+                    <li><a class="waves-effect btn" id="login-button">Einloggen</a></li>
+                    <li><a class="waves-effect waves-light btn">Registrieren</a></li>
+                <?php
+                }else{
+                ?>
+                    <li><a class="waves-effect waves-light btn">Aufgabe erstellen</a></li>
+                    <li>
+                        <div class="avatar-container">
+                            <a class="avatar" href="#">
+                                <img class="avatar" src="/assets/pp_default.svg">
+                            </a>
+                        </div>
+                    </li>
+                <?php
+                }
+                ?>
+            </ul>
+        </div>
+    </nav>
+</body>
