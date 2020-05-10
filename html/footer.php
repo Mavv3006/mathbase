8f200dc25f2f0906bbbdbe09b697bec28308d132
Luisa Fuss <luisa.fuss@web.de>
2020-05-10 11:59:58 +0200

    footer added

--- html/footer.php
+++ html/footer.php
@@ -1,8 +1,13 @@
-<link rel="stylesheet" href="/css/footer.css"/>
-<footer class="page-footer">
+<head>
+  <link rel="stylesheet" href="/css/footer.css" />
+</head>
+
+<footer class="mb-white-text page-footer">
   <div class="footer-copyright">
-    &copy; Mathbase
-    <?php echo date("Y"); ?>
-  </div>
+    <div class="container">
+      &copy; Mathbase
+      <?php echo date("Y"); ?>
+      <a class="mb-white-text right" href="#!">Datenschutz</a>
+    </div>
   </div>
 </footer>
