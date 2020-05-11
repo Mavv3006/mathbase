1fb5094874650840c2a3b55c520a9dc0d4b13f3b
unknown <florian.goelz@outlook.de>
2020-05-11 16:08:40 +0200

    added input fields

--- html/profile.php
+++ html/profile.php
@@ -13,20 +13,30 @@
         <div class="col s6 offset-s3">
             <div class="card" id="profile">
                 <span class="card-title" id="profile-title">Profil</span>
-                <div class="card-content">
-                    <div id="profile-avatar-container">
+                <div id="profile-avatar-container">
                         <img id="profile-avatar" src="/assets/pp_default.svg">
-                        <div id="avatar-button">
-                            <a class="waves-effect waves-light btn">Bild hochladen</a><br>
+                        <div id="avatar-button-container">
+                            <a class="waves-effect waves-light btn" id="avatar-button">Bild hochladen</a><br>
                             maximal 1MB
                         </div>
                     </div>
-                    <div class="input-field col">
+                <div class="card-content">
+                    <div class="input-field col s10">
                         <input id="email" type="email" class="validate">
                         <label for="email">E-Mail-Adresse</label>
+                    </div>
+                    <a class="waves-effect waves-light btn"></a>
+                    <div class="input-field col s10">
+                        <input id="username" type="text" class="validate">
+                        <label for="username">Benutzername</label>
+                    </div>
+                    <div class="input-field col s10">
+                        <input id="password" type="password" class="validate">
+                        <label for="password">E-Mail-Adresse</label>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </body>
+<script src="../js/bin/materialize.min.js"></script>
