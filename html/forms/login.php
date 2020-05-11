65cc8eadb340acfc214830343e63bfb2fb5fb0c0
Luisa Fuss <luisa.fuss@web.de>
2020-05-11 14:33:50 +0200

    Login as Card

--- html/forms/login.php
+++ html/forms/login.php
@@ -1,29 +1,36 @@
-<form method="POST" action="#">
-    <h2>Einloggen</h2>
 
+<div class="row" style="align-items:center;height:100%;">
+    <div class="col s4 offset-s4">
+        <div class="card white">
+            <div class="card-content mb-prussian-blue-text">
+                <span class="card-title">
+                    <h2>Einloggen</h2>
+                </span>
+                <form method="POST" action="#">
 
-    <div class="input-field">
-        <input id="username" type="text">
-        <label for="username">Benutername</label>
+                    <div class="input-field">
+                        <input id="username" type="text">
+                        <label for="username">Benutername</label>
+                    </div>
+
+                    <div class="input-field">
+                        <input id="password" type="password">
+                        <label for="password">Passwort</label>
+                    </div>
+
+                    <p style="padding-bottom: 19px">
+                        <a href="#">
+                            Ich habe mein Passwort vergessen
+                        </a>
+                    </p>
+
+                    <button class="btn waves-effect waves-light" type="submit" name="action">Einloggen</button>
+
+                </form>
+            </div>
+            <div class="card-action">
+                <a href="#">Noch kein Mitglied? <b>Jetzt registrieren!</b></a>
+            </div>
+        </div>
     </div>
-
-    <div class="input-field">
-        <input id="password" type="password">
-        <label for="password">Passwort</label>
-    </div>
-
-    <p>
-        <a href="#">
-            Ich habe mein Passwort vergessen
-        </a>
-    </p>
-
-    <button class="btn waves-effect waves-light" type="submit" name="action">Einloggen</button>
-
-    <p>
-        <a href="#">
-            Noch kein Mitglied? <b>Jetzt registrieren!</b>
-        </a>
-    </p>
-
-</form>
+</div>
