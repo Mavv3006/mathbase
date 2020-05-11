d521ed5b6cd5754c51dda935bc5bed89e7a0443c
unknown <florian.goelz@outlook.de>
2020-05-11 17:20:52 +0200

    added buttons

--- html/profile.php
+++ html/profile.php
@@ -14,26 +14,35 @@
             <div class="card" id="profile">
                 <span class="card-title" id="profile-title">Profil</span>
                 <div id="profile-avatar-container">
-                        <img id="profile-avatar" src="/assets/pp_default.svg">
-                        <div id="avatar-button-container">
-                            <a class="waves-effect waves-light btn" id="avatar-button">Bild hochladen</a><br>
-                            maximal 1MB
-                        </div>
+                    <img id="profile-avatar" src="/assets/pp_default.svg">
+                    <div id="avatar-button-container">
+                        <a class="waves-effect waves-light btn" id="avatar-button">Bild hochladen</a><br>
                     </div>
+                </div>
                 <div class="card-content">
-                    <div class="input-field col s10">
-                        <input id="email" type="email" class="validate">
-                        <label for="email">E-Mail-Adresse</label>
-                    </div>
-                    <a class="waves-effect waves-light btn"></a>
-                    <div class="input-field col s10">
-                        <input id="username" type="text" class="validate">
-                        <label for="username">Benutzername</label>
-                    </div>
-                    <div class="input-field col s10">
-                        <input id="password" type="password" class="validate">
-                        <label for="password">E-Mail-Adresse</label>
-                    </div>
+                    <form method="UPDATE" action="#">
+                        <div class="input-field col s10 profile-input">
+                            <input id="email" type="email" class="validate">
+                            <label for="email">E-Mail-Adresse</label>
+                        </div>
+                        <a class="edit-button waves-effect waves-light btn" href="#">
+                            <i class="edit-icon material-icons">create</i>
+                        </a>
+                        <div class="input-field col s10">
+                            <input id="username" type="text" class="validate">
+                            <label for="username">Benutzername</label>
+                        </div>
+                        <a class="edit-button waves-effect waves-light btn" href="#">
+                            <i class="edit-icon material-icons">create</i>
+                        </a>
+                        <div class="input-field col s10">
+                            <input id="password" type="password" class="validate">
+                            <label for="password">E-Mail-Adresse</label>
+                        </div>
+                        <a class="edit-button waves-effect waves-light btn" href="#">
+                            <i class="large material-icons">create</i>
+                        </a>
+                    </form>
                 </div>
             </div>
         </div>
