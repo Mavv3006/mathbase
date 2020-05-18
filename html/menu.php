25ac665ad29e825651843af854fcac0ad24e18c9
Luisa Fuss <luisa.fuss@web.de>
2020-05-18 15:37:48 +0200

    Pfeil näher an Bild

--- html/menu.php
+++ html/menu.php
@@ -1,15 +1,17 @@
 <head>
     <link rel="stylesheet" href="/css/menu.css" />
-</head> 
+</head>
 
+<div class="menu">
+    <!-- Dropdown Structure -->
+    <ul id="userMenu" class="dropdown-content">
+        <li><span>Hallo <b>Nutzer!</b></span></li>
+        <li class="divider"></li>
+        <li><a href="#!">Profil</a></li>
+        <li><a href="#!">Ausloggen</a></li>
+    </ul>
+</div>
 
-<!-- Dropdown Structure -->
-<ul id="userMenu" class="dropdown-content">
-    <li><span>Hallo <b>Nutzer!<b></span></li>
-    <li class="divider"></li>
-    <li><a href="#!">Profil</a></li>
-    <li><a href="#!">Ausloggen</a></li>
-</ul>
 
 
 <script>
