94a0ec4670c4fc6b9bcad682c273cd7d07ff72f2
unknown <florian.goelz@outlook.de>
2020-05-18 15:16:50 +0200

    added disableInput function

--- html/profile.php
+++ html/profile.php
@@ -5,48 +5,58 @@ include  $_SERVER['DOCUMENT_ROOT'] . '/html/head.php';
 
 <head>
     <link rel="stylesheet" href="/css/profile.css" />
-    <link rel="stylesheet" href="/assets/icons/iconfont/material-icons.css" </head> <body>
+    <link rel="stylesheet" href="/assets/icons/iconfont/material-icons.css"
     <?php
     include $_SERVER['DOCUMENT_ROOT'] . '/html/header.php';
     ?>
 </head>
-<div class="row">
-    <div class="col s6 offset-s3">
-        <div class="card" id="profile">
-            <span class="card-title" id="profile-title">Profil</span>
-            <div id="profile-avatar-container">
-                <img id="profile-avatar" src="/assets/pp_default.svg">
-                <div id="avatar-button-container">
-                    <a class="waves-effect waves-light btn" id="avatar-button">Bild hochladen</a><br>
+<body>
+    <div class="row">
+        <div class="col s4 offset-s3">
+            <div class="card" id="profile">
+                <span class="card-title" id="profile-title">Profil</span>
+                <div id="profile-avatar-container">
+                    <img id="profile-avatar" src="/assets/pp_default.svg">
+                    <div id="avatar-button-container">
+                        <a class="waves-effect waves-light btn" id="avatar-button">Bild hochladen</a><br>
+                    </div>
                 </div>
-            </div>
-            <div class="card-content">
-                <form method="UPDATE" action="#">
-                    <div class="input-field col s10 profile-input">
-                        <input id="email" type="email" class="validate">
-                        <label for="email">E-Mail-Adresse</label>
-                    </div>
-                    <a class="edit-button waves-effect waves-light btn" href="#">
-                        <i class="material-icons" id="edit">create</i>
-                    </a>
-                    <div class="input-field col s10">
-                        <input id="username" type="text" class="validate">
-                        <label for="username">Benutzername</label>
-                    </div>
-                    <a class="edit-button waves-effect waves-light btn" href="#">
-                        <i class="material-icons">create</i>
-                    </a>
-                    <div class="input-field col s10">
-                        <input id="password" type="password" class="validate">
-                        <label for="password">E-Mail-Adresse</label>
-                    </div>
-                    <a class="edit-button waves-effect waves-light btn" href="#">
-                        <i class="material-icons">create</i>
-                    </a>
-                </form>
+                <div class="card-content">
+                    <form method="UPDATE" action="#">
+                        <div class="input-field col s10 profile-input">
+                            <input disabled id="email" type="email" class="validate">
+                            <label for="email">E-Mail-Adresse</label>
+                        </div>
+                        <a class="edit-button waves-effect waves-light btn" onclick="disableInput('email')">
+                            <i class="material-icons" id="edit">create</i>
+                        </a>
+                        <div class="input-field col s10">
+                            <input disabled id="username" type="text" class="validate">
+                            <label for="username">Benutzername</label>
+                        </div>
+                        <a class="edit-button waves-effect waves-light btn" onclick="disableInput('username')">
+                            <i class="material-icons">create</i>
+                        </a>
+                        <div class="input-field col s10">
+                            <input disabled id="password" type="password" class="validate">
+                            <label for="password">E-Mail-Adresse</label>
+                        </div>
+                        <a class="edit-button waves-effect waves-light btn" onclick="disableInput('password')">
+                            <i class="material-icons">create</i>
+                        </a>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
-</div>
 </body>
+<script>
+    function disableInput(element){
+        if(document.getElementById(element).disabled){
+            document.getElementById(element).disabled = false;
+        }else{
+            document.getElementById(element).disabled = true;
+        }
+    }
+</script>
 <script src="../js/bin/materialize.min.js"></script>
