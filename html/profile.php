6e8c61dc4b9886df20037dff604e9c37bbdd327c
Luisa Fuss <luisa.fuss@web.de>
2020-05-24 22:30:29 +0200

    fixed footer-position on profile page

--- html/profile.php
+++ html/profile.php
@@ -14,39 +14,41 @@ setUserLocation("profile");
 </head>
 
 <body>
-    <div class="row">
-        <div class="col m6 offset-m3">
-            <div class="card" id="profile">
-                <span class="card-title" id="profile-title">Profil</span>
-                <div id="profile-avatar-container">
-                    <img id="profile-avatar" src="/assets/pp_default.svg">
-                    <a class="waves-effect waves-light btn" id="avatar-button">Bild hochladen</a><br>
-                </div>
-                <div class="card-content">
-                    <form method="UPDATE" action="#">
-                        <div class="input-field col s10 profile-input">
-                            <input disabled id="email" type="email" class="validate">
-                            <label for="email">E-Mail-Adresse</label>
-                        </div>
-                        <a class="edit-button waves-effect waves-light btn" onclick="disableInput('email')">
-                            <i class="material-icons" id="edit">create</i>
-                        </a>
-                        <div class="input-field col s10">
-                            <input disabled id="username" type="text" class="validate">
-                            <label for="username">Benutzername</label>
-                        </div>
-                        <a class="edit-button waves-effect waves-light btn" onclick="disableInput('username')">
-                            <i class="material-icons">create</i>
-                        </a>
-                        <div class="input-field col s10">
-                            <input disabled id="password" type="password" class="validate">
-                            <label for="password">Passwort</label>
-                        </div>
-                        <a class="edit-button waves-effect waves-light btn" onclick="disableInput('password')">
-                            <i class="material-icons">create</i>
-                        </a>
-                    </form>
-                    <a class="waves-effect waves-light btn" id="save-button">Änderungen speichern</a>
+    <div class="container">
+        <div class="row">
+            <div class="col m6 offset-m3">
+                <div class="card" id="profile">
+                    <span class="card-title" id="profile-title">Profil</span>
+                    <div id="profile-avatar-container">
+                        <img id="profile-avatar" src="/assets/pp_default.svg">
+                        <a class="waves-effect waves-light btn" id="avatar-button">Bild hochladen</a><br>
+                    </div>
+                    <div class="card-content">
+                        <form method="UPDATE" action="#">
+                            <div class="input-field col s10 profile-input">
+                                <input disabled id="email" type="email" class="validate">
+                                <label for="email">E-Mail-Adresse</label>
+                            </div>
+                            <a class="edit-button waves-effect waves-light btn" onclick="disableInput('email')">
+                                <i class="material-icons" id="edit">create</i>
+                            </a>
+                            <div class="input-field col s10">
+                                <input disabled id="username" type="text" class="validate">
+                                <label for="username">Benutzername</label>
+                            </div>
+                            <a class="edit-button waves-effect waves-light btn" onclick="disableInput('username')">
+                                <i class="material-icons">create</i>
+                            </a>
+                            <div class="input-field col s10">
+                                <input disabled id="password" type="password" class="validate">
+                                <label for="password">Passwort</label>
+                            </div>
+                            <a class="edit-button waves-effect waves-light btn" onclick="disableInput('password')">
+                                <i class="material-icons">create</i>
+                            </a>
+                        </form>
+                        <a class="waves-effect waves-light btn" id="save-button">Änderungen speichern</a>
+                    </div>
                 </div>
             </div>
         </div>
