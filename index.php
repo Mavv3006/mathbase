249e8b50b33c1cdb3cb0d5d21c6b8d26c293a771
Luisa Fuss <luisa.fuss@web.de>
2020-05-24 20:20:27 +0200

    fixed wrong position of footer on index-page

--- index.php
+++ index.php
@@ -10,72 +10,73 @@ include  $_SERVER['DOCUMENT_ROOT'] . '/html/head.php';
 <body>
     <?php include $_SERVER['DOCUMENT_ROOT'] . '/html/header.php'; ?>
 
+    <div class="introduction">
 
-    <div class="title">
-        <h1><b>Mathe</b> üben so einfach wie nie.</h1>
-        <h5>So viel Spaß hat Lernen selten gemacht!</h5>
+        <div class="title">
+            <h1><b>Mathe</b> üben so einfach wie nie.</h1>
+            <h5>So viel Spaß hat Lernen selten gemacht!</h5>
+        </div>
+
+        <div class="row">
+            <div class="col s10 offset-s3 m2 offset-m2">
+                <div class="card">
+                    <div class="card-image mb-orange-text waves-effect waves-block waves-light">
+                        <i class="large material-icons">access_time</i>
+                    </div>
+                    <div class="card-content">
+                        <span class="card-title">Jederzeit</span>
+                        <p>Schulstunden gehen 45 Minuten - Diese Plattform steht Dir 24/7 zur Verfügung.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col s12 m2">
+                <div class="card">
+                    <div class="card-image mb-orange-text waves-effect waves-block waves-light">
+                        <i class="large material-icons">beach_access</i>
+                    </div>
+                    <div class="card-content">
+                        <span class="card-title">Überall</span>
+                        <p>Ob am Schreibtisch oder unter Palmen - Du kannst überall lernen.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col s12 m2">
+                <div class="card">
+                    <div class="card-image mb-orange-text waves-effect waves-block waves-light">
+                        <i class="large material-icons">fitness_center</i>
+                    </div>
+                    <div class="card-content">
+                        <span class="card-title">Ohne Druck</span>
+                        <p>Nimm Dir die Zeit, die Du brauchst - Keiner sagt Dir, wie schnell Du fit sein musst.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col s12 m2">
+                <div class="card">
+                    <div class="card-image mb-orange-text waves-effect waves-block waves-light">
+                        <i class="large material-icons">redeem</i>
+                    </div>
+                    <div class="card-content">
+                        <span class="card-title">Kostenlos</span>
+                        <p>Lernen sollte nichts mit Geld zu tun haben - Deswegen kannst du bei uns kostenlos Mathe üben.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <h5>
+            Teste jetzt <b><span class="mb-prussian-blue-text">Math</span><span class="mb-orange-text">base</span></b> und werde zum Mathe-Champion!
+        </h5>
+
+        <div class="button-start">
+            <a class="waves-effect waves-light btn" href="/main.php">Jetzt starten</a>
+        </div>
+
     </div>
-
-    <div class="row">
-        <div class="col s10 offset-s3 m2 offset-m2">
-            <div class="card">
-                <div class="card-image mb-orange-text waves-effect waves-block waves-light">
-                    <i class="large material-icons">access_time</i>
-                </div>
-                <div class="card-content">
-                    <span class="card-title">Jederzeit</span>
-                    <p>Schulstunden gehen 45 Minuten - Diese Plattform steht Dir 24/7 zur Verfügung.</p>
-                </div>
-            </div>
-        </div>
-
-        <div class="col s12 m2">
-            <div class="card">
-                <div class="card-image mb-orange-text waves-effect waves-block waves-light">
-                    <i class="large material-icons">beach_access</i>
-                </div>
-                <div class="card-content">
-                    <span class="card-title">Überall</span>
-                    <p>Ob am Schreibtisch oder unter Palmen - Du kannst überall lernen.</p>
-                </div>
-            </div>
-        </div>
-
-        <div class="col s12 m2">
-            <div class="card">
-                <div class="card-image mb-orange-text waves-effect waves-block waves-light">
-                    <i class="large material-icons">fitness_center</i>
-                </div>
-                <div class="card-content">
-                    <span class="card-title">Ohne Druck</span>
-                    <p>Nimm Dir die Zeit, die Du brauchst - Keiner sagt Dir, wie schnell Du fit sein musst.</p>
-                </div>
-            </div>
-        </div>
-
-        <div class="col s12 m2">
-            <div class="card">
-                <div class="card-image mb-orange-text waves-effect waves-block waves-light">
-                    <i class="large material-icons">redeem</i>
-                </div>
-                <div class="card-content">
-                    <span class="card-title">Kostenlos</span>
-                    <p>Lernen sollte nichts mit Geld zu tun haben - Deswegen kannst du bei uns kostenlos Mathe üben.</p>
-                </div>
-            </div>
-        </div>
-    </div>
-
-
-    <h5>
-        Teste jetzt <b><span class="mb-prussian-blue-text">Math</span><span class="mb-orange-text">base</span></b> und werde zum Mathe-Champion!
-    </h5>
-
-    <div class="button-start">
-        <a class="waves-effect waves-light btn" href="/main.php">Jetzt starten</a>
-    </div>
-
-
 
     <?php include $_SERVER['DOCUMENT_ROOT'] . '/html/footer.php'; ?>
 </body>
