e5fa2f26a6fb07f0ff48292363573c03225a48cf
Florian Goelz <florian.goelz@outlook.de>
2020-06-20 16:13:59 +0200

    added filter form fields

--- src/html/exercise_list.php
+++ src/html/exercise_list.php
@@ -12,6 +12,8 @@ $subcategoryViewModel = new SubCategoryViewModel();
 $categoryViewModel = new CategoryViewModel();
 
 $exercises = $exerciseViewModel->get_all();
+$categories = $categoryViewModel->get_all();
+$subcategories = $subcategoryViewModel->get_all();
 
 $site_name = "Aufgabenliste";
 require_once($path['src'] . '/html/head.php');
@@ -28,6 +30,28 @@ require_once($path['src'] . '/html/head.php');
         <h1>Neue Aufgaben</h1>
     </div>
 
+    <div class="row form" method="post">
+        <div class="input-field col s12 m5">
+            <select multiple name="categories[]" id="category-filter">
+                <?php foreach($categories as $category){ ?>
+                <option value="<?= $category->get_id() ?>"><?= $category->get_description() ?></option>
+                <?php } ?>
+            </select>
+            <label>Kategorie</label>
+        </div>
+        <div class="input-field col s12 m5">
+            <select multiple name="subcategory-filter" id="subcategory-filter">
+                <?php foreach($subcategories as $subcategory){ ?>
+                <option value="<?= $subcategory->get_id() ?>"><?= $subcategory->get_description() ?></option>
+                <?php } ?>
+            </select>
+            <label>Unterkategorie</label>
+        </div>
+        <div class="col s12 m2">
+            <a href="main.php?filter=true" class="waves-effect waves-light btn" onclick="">Filtern</a>
+        </div>
+    </div>
+    
     <hr>
 
     <div class="exercise-grid">
@@ -35,6 +59,9 @@ require_once($path['src'] . '/html/head.php');
             $difficulty = $difficultyViewModel->get_by_id($exercise->get_difficulty());
             $category = $categoryViewModel->get_by_id($exercise->get_category());
             $subcategory = $subcategoryViewModel->get_by_id($exercise->get_subcategory());
+            $selected_categories = array();
+            
+            if(count($selected_categories) == 0 || in_array($category, $selected_categories)){
         ?>
             <a href=" <?= 'exercise.php?id=' . $exercise->get_id() ?>">
                 <div class="exercise hoverable">
@@ -48,7 +75,25 @@ require_once($path['src'] . '/html/head.php');
                     <p class="difficulty col-content"><?= $difficulty->get_description() ?></p>
                 </div>
             </a>
-        <?php } ?>
+        <?php }
+        } ?>
     </div>
 
 </div>
+<?php
+
+    if(isset($_GET['filter'])){
+        getSelectedCategories();
+    }
+
+    function getSelectedCategories(){
+        foreach($_POST['categories'] as $selected_category){
+            array_push($selected_categories, $selected_category);
+        }
+    }
+?>
+<script> 
+    $(document).ready(function(){
+        $('select').formSelect();
+    });
+</script>
