39614ec5f0a301f0afc6e2dbbecad54681a1b65f
Mavv <marvdeus@gmail.com>
2020-06-21 21:55:20 +0200

    added default

--- src/html/exercise_list.php
+++ src/html/exercise_list.php
@@ -34,16 +34,18 @@ require_once($path['src'] . '/html/head.php');
         <form method="post">
             <div class="input-field col s12 m5">
                 <select multiple name="categories[]" id="category-filter">
-                    <?php foreach($categories as $category){ ?>
-                    <option value="<?= $category->get_id() ?>"><?= $category->get_description() ?></option>
+                    <option value="" disabled selected>Alle Kategorien</option>
+                    <?php foreach ($categories as $category) { ?>
+                        <option value="<?= $category->get_id() ?>"><?= $category->get_description() ?></option>
                     <?php } ?>
                 </select>
                 <label>Kategorie</label>
             </div>
             <div class="input-field col s12 m5">
                 <select multiple name="subcategories[]" id="subcategory-filter">
-                    <?php foreach($subcategories as $subcategory){ ?>
-                    <option value="<?= $subcategory->get_id() ?>"><?= $subcategory->get_description() ?></option>
+                    <option value="" disabled selected>Alle Unterkategorien</option>
+                    <?php foreach ($subcategories as $subcategory) { ?>
+                        <option value="<?= $subcategory->get_id() ?>"><?= $subcategory->get_description() ?></option>
                     <?php } ?>
                 </select>
                 <label>Unterkategorie</label>
@@ -53,23 +55,23 @@ require_once($path['src'] . '/html/head.php');
             </div>
         </form>
     </div>
-    
+
     <hr>
 
     <div class="exercise-grid">
-        <?php 
+        <?php
         $selected_categories = array();
         $selected_subcategories = array();
 
-        if(isset($_POST['submit'])){
-            if(isset($_POST['categories'])){
-                foreach($_POST['categories'] as $selected_category){
+        if (isset($_POST['submit'])) {
+            if (isset($_POST['categories'])) {
+                foreach ($_POST['categories'] as $selected_category) {
                     array_push($selected_categories, $selected_category);
                 }
             }
 
-            if(isset($_POST['subcategories'])){
-                foreach($_POST['subcategories'] as $selected_subcategory){
+            if (isset($_POST['subcategories'])) {
+                foreach ($_POST['subcategories'] as $selected_subcategory) {
                     array_push($selected_subcategories, $selected_subcategory);
                 }
             }
@@ -79,29 +81,29 @@ require_once($path['src'] . '/html/head.php');
             $difficulty = $difficultyViewModel->get_by_id($exercise->get_difficulty());
             $category = $categoryViewModel->get_by_id($exercise->get_category());
             $subcategory = $subcategoryViewModel->get_by_id($exercise->get_subcategory());
-            
-            if((count($selected_categories) == 0 || in_array($category->get_id(), $selected_categories) && 
-                (count($selected_subcategories) == 0 || in_array($subcategory->get_id(), $selected_subcategories)))){
+
+            if ((count($selected_categories) == 0 || in_array($category->get_id(), $selected_categories) &&
+                (count($selected_subcategories) == 0 || in_array($subcategory->get_id(), $selected_subcategories)))) {
         ?>
-            <a href=" <?= 'exercise.php?id=' . $exercise->get_id() ?>">
-                <div class="exercise hoverable">
-                    <p class="category col-content">
-                        <span class="main_category"><?= $category->get_description() ?></span>
-                        -
-                        <span class="sub_category"><?= $subcategory->get_description() ?></span>
-                    </p>
-                    <p class="title col-content"><?= $exercise->get_title() ?></p>
-                    <div class="short-text col-content"><?= $exercise->get_description() ?></div>
-                    <p class="difficulty col-content"><?= $difficulty->get_description() ?></p>
-                </div>
-            </a>
+                <a href=" <?= 'exercise.php?id=' . $exercise->get_id() ?>">
+                    <div class="exercise hoverable">
+                        <p class="category col-content">
+                            <span class="main_category"><?= $category->get_description() ?></span>
+                            -
+                            <span class="sub_category"><?= $subcategory->get_description() ?></span>
+                        </p>
+                        <p class="title col-content"><?= $exercise->get_title() ?></p>
+                        <div class="short-text col-content"><?= $exercise->get_description() ?></div>
+                        <p class="difficulty col-content"><?= $difficulty->get_description() ?></p>
+                    </div>
+                </a>
         <?php }
         } ?>
     </div>
 
 </div>
-<script> 
-    $(document).ready(function(){
+<script>
+    $(document).ready(function() {
         $('select').formSelect();
     });
 </script>
